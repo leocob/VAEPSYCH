implement correct scaling on train and test separately

src/move/data/io.py
@@ -129,7 +129,7 @@ def read_names(path: PathLike) -> list[str]:
 
 
 def read_tsv(
-    path: PathLike, sample_names: Optional[list[str]] = None
+    path: PathLike, sample_names: Optional[list[str]] = None, input_type: str = "categorical", p: float = 0.01
 ) -> tuple[ObjectArray, np.ndarray]:
     """Read a dataset from a TSV file. The TSV is expected to have an index
     column (0th index).
@@ -146,6 +146,38 @@ def read_tsv(
     if sample_names is not None:
         data.index = data.index.astype(str, False)
         data = data.loc[sample_names]
+
+    if input_type == "categorical":
+        percentage_of_ones = (data == 1).mean()
+        columns_to_keep = percentage_of_ones[percentage_of_ones >= p].index
+        data = data[columns_to_keep]
+        columns_removed = percentage_of_ones[percentage_of_ones < p].index
+
+        # transform index to list
+        columns_to_keep = columns_to_keep.tolist()
+        # write list to file
+        with open(f"{path}_features_kept_more_than_{p}.txt", "w") as file:
+            for column in columns_to_keep:
+                file.write(f"{column}\n")
+
+
+        columns_to_remove = columns_removed.tolist()
+        with open(f"{path}_features_removed_less_than_{p}.txt", "w") as file:
+            for column in columns_to_remove:
+                file.write(f"{column}\n")
+
+    elif input_type == "continuous":
+        percentage_of_nonas = data.notna().mean()
+        columns_to_keep = percentage_of_nonas[percentage_of_nonas >= p].index
+        data = data[columns_to_keep]
+        columns_removed = percentage_of_nonas[percentage_of_nonas < p].index
+
+        columns_removed = columns_removed.tolist()
+
+        with open(f"{path}_features_removed_less_than_{p}.txt", "w") as file:
+            for column in columns_removed:
+                file.write(f"{column}\n")
+
     return data.columns.values, data.values
 
 

src/move/data/preprocessing.py
@@ -6,6 +6,13 @@ import numpy as np
 import pandas as pd
 from numpy.typing import ArrayLike
 from sklearn.preprocessing import scale as standardize
+from sklearn.preprocessing import StandardScaler
+from sklearn.impute import SimpleImputer
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy import stats
+from math import ceil
+
 
 from move.core.typing import BoolArray, FloatArray, IntArray
 
@@ -64,7 +71,23 @@ def one_hot_encode_single(mapping: dict[str, int], value: Optional[str]) -> IntA
     return encoded_value
 
 
-def scale(x: np.ndarray) -> tuple[FloatArray, BoolArray]:
+# def scale(x: np.ndarray) -> tuple[FloatArray, BoolArray]:
+#     """Center to mean and scale to unit variance. Convert NaN values to 0.
+
+#     Args:
+#         x: 2D array with samples in its rows and features in its columns
+
+#     Returns:
+#         Tuple containing (1) scaled output and (2) a 1D mask marking columns
+#         (i.e., features) without zero variance
+#     """
+#     logx = np.log2(x + 1)
+#     mask_1d = ~np.isclose(np.nanstd(logx, axis=0), 0.0)
+#     scaled_x = standardize(logx[:, mask_1d], axis=0)
+#     scaled_x[np.isnan(scaled_x)] = 0
+#     return scaled_x, mask_1d
+
+def scale(x: np.array, split_mask, names, interim_data_path, input_config_name) -> tuple[FloatArray, BoolArray]:
     """Center to mean and scale to unit variance. Convert NaN values to 0.
 
     Args:
@@ -74,8 +97,81 @@ def scale(x: np.ndarray) -> tuple[FloatArray, BoolArray]:
         Tuple containing (1) scaled output and (2) a 1D mask marking columns
         (i.e., features) without zero variance
     """
-    logx = np.log2(x + 1)
-    mask_1d = ~np.isclose(np.nanstd(logx, axis=0), 0.0)
-    scaled_x = standardize(logx[:, mask_1d], axis=0)
-    scaled_x[np.isnan(scaled_x)] = 0
+    x_train = x[split_mask]
+    x_test = x[~split_mask]
+
+    # Do I want a log transformation?
+    logx_train = np.log2(x_train + 1)
+    logx_test = np.log2(x_test + 1)
+
+
+    mask_1d = ~np.isclose(np.nanstd(logx_train, axis=0), 0.0)
+    
+    imputer = SimpleImputer(strategy='mean')
+    scaler = StandardScaler()
+
+    scaled_x_train = scaler.fit_transform(imputer.fit_transform(logx_train[:, mask_1d]))
+    scaled_x_test = scaler.transform(imputer.transform(logx_test[:, mask_1d]))
+
+    scaled_x = np.concatenate((scaled_x_train, scaled_x_test), axis=0)
+
+    # print mean of means
+
+    print(f"Mean of means of scaled_x_train {scaled_x_train.mean(axis=0).mean()}")
+    print(f"Mean of stds of scaled_x_train {scaled_x_train.std(axis=0).mean()}")
+    print(f"Mean of means of scaled_x_test {scaled_x_test.mean(axis=0).mean()}")
+    print(f"Mean of stds of scaled_x_test {scaled_x_test.std(axis=0).mean()}")
+    print(f"Mean of means of scaled_x {scaled_x.mean(axis=0).mean()}")
+    print(f"Mean of stds of scaled_x {scaled_x.std(axis=0).mean()}")
+ 
+    # plot_distr(x_train, logx_train, scaled_x_train, names, interim_data_path, input_config_name)
+    
     return scaled_x, mask_1d
+
+
+
+# def plot_distr(data_before_log, data_after_log, data_after_log_scaled, names, interim_data_path, input_config_name):
+
+#     n_features = data_before_log.shape[1]
+#     n_cols = 3
+#     n_rows = 5  # Number of features to plot per page
+#     n_pages = ceil(n_features / n_rows)
+
+#     for page in range(n_pages):
+#         fig, axes = plt.subplots(n_rows, n_cols, figsize=(15, 5*n_rows))
+#         fig.suptitle(f'Distribution of features (Page {page+1}/{n_pages})', fontsize=16)
+
+#         for i in range(n_rows):
+#             feature_idx = page * n_rows + i
+#             if feature_idx >= n_features:
+#                 break
+
+#             feature_name = names[feature_idx]
+
+#             # Before log transformation
+#             sns.histplot(data_before_log[:, feature_idx], kde=True, ax=axes[i, 0])
+#             axes[i, 0].set_title(f'{feature_name}\nBefore Log')
+#             axes[i, 0].set_xlabel('Value')
+
+#             # After log transformation
+#             sns.histplot(data_after_log[:, feature_idx], kde=True, ax=axes[i, 1])
+#             axes[i, 1].set_title(f'{feature_name}\nAfter Log')
+#             axes[i, 1].set_xlabel('Value')
+
+#             # After scaling
+#             sns.histplot(data_after_log_scaled[:, feature_idx], kde=True, ax=axes[i, 2])
+#             axes[i, 2].set_title(f'{feature_name}\nAfter Scaling')
+#             axes[i, 2].set_xlabel('Value')
+
+#         # Remove any unused subplots
+#         for i in range(feature_idx % n_rows + 1, n_rows):
+#             for j in range(n_cols):
+#                 fig.delaxes(axes[i, j])
+
+#         plt.tight_layout()
+#         plt.savefig(f'{interim_data_path}/{input_config_name}_page{page+1}.pdf')
+#         plt.close()
+
+#     print(f"Plots saved to {interim_data_path}/{input_config_name}_page*.pdf")
+
+#     return None

src/move/tasks/encode_data.py
@@ -3,6 +3,7 @@ __all__ = ["encode_data"]
 from pathlib import Path
 
 import numpy as np
+import pandas as pd
 
 from move.conf.schema import DataConfig
 from move.core.logging import get_logger
@@ -27,11 +28,29 @@ def encode_data(config: DataConfig):
 
     sample_names = io.read_names(raw_data_path / f"{config.sample_names}.txt")
 
+
+    train_test_path = raw_data_path / "train_test_splits.tsv"
+
+    if not train_test_path.exists():
+        raise FileNotFoundError(f"Train test split file not found: {train_test_path}")
+
+    # I need to create the split mask
+    split_path = interim_data_path / "split_mask.npy"
+
+    # Will read train test split everytime I run the encoding_data, even if the split mask is already there
+    # So I'm sure that the split mask is always the most recent one, as long as I don't move the train_test_splits.tsv file
+
+    train_test_splits = pd.read_csv(train_test_path, sep = "\t")
+    split_mask = train_test_splits["Split"].values == "Train"
+    np.save(split_path, split_mask)
+
+
     mappings = {}
     for dataset_name in config.categorical_names:
         logger.info(f"Encoding '{dataset_name}'")
         filepath = raw_data_path / f"{dataset_name}.tsv"
-        names, values = io.read_tsv(filepath, sample_names)
+        names, values = io.read_tsv(filepath, sample_names, input_type = "categorical", p = 0.01)
+
         values, mapping = preprocessing.one_hot_encode(values)
         mappings[dataset_name] = mapping
         io.dump_names(interim_data_path / f"{dataset_name}.txt", names)
@@ -44,10 +63,17 @@ def encode_data(config: DataConfig):
         action_name = "Encoding" if scale else "Reading"
         logger.info(f"{action_name} '{input_config.name}'")
         filepath = raw_data_path / f"{input_config.name}.tsv"
-        names, values = io.read_tsv(filepath, sample_names)
+        names, values = io.read_tsv(filepath, sample_names, input_type = "continuous", p = 0.01)
+
+
         if scale:
-            values, mask_1d = preprocessing.scale(values)
+            input_config_name = input_config.name
+            values, mask_1d = preprocessing.scale(values, split_mask, names, interim_data_path, input_config_name)
+            # values, mask_1d = preprocessing.scale(values, split_mask)
             names = names[mask_1d]
             logger.debug(f"Columns with zero variance: {np.sum(~mask_1d)}")
         io.dump_names(interim_data_path / f"{input_config.name}.txt", names)
         np.save(interim_data_path / f"{input_config.name}.npy", values)
+
+
+