446f5b78e1b073ec31af9ffe079e6bf81f4617d8
leocob <leonardo.cobuccio@cpr.ku.dk>
2024-07-14 16:17:53 +0200

    print df

--- src/move/data/preprocessing.py
+++ src/move/data/preprocessing.py
@@ -149,7 +149,8 @@ def scale(x: np.array, train_test_splits, split_mask, names, interim_data_path, 
         # print index of scaled_x
         # convert scaled_x to pandas dataframe
         scaled_x_df = pd.DataFrame(scaled_x, columns=names[mask_1d])
-        print(scaled_x_df[:,0:4798])
+        # print scaled_x_df from index 0 to 4799
+        print(f"scaled_x_df: \n{scaled_x_df.iloc[:4800]}")
         print(f"scaled_x_df: \n{scaled_x_df.query('index == 4797')}")
         # print(f"scaled_x_df[5997,:]: {scaled_x_df.iloc[5997,:]}")
         # print(f"scaled_x: {scaled_x}")
