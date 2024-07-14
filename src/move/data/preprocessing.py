8282e7f310b473d9f5ea5be4505c123e88afe025
leocob <leonardo.cobuccio@cpr.ku.dk>
2024-07-14 17:41:01 +0200

    print

--- src/move/data/preprocessing.py
+++ src/move/data/preprocessing.py
@@ -237,6 +237,7 @@ def scale(x: np.array, data, train_test_splits, split_mask, names, interim_data_
 
         print(f"scaled_x_df.query(ID==1219925):")
         print(scaled_x_df.query("ID == 1219925"))
+        print(scaled_x["ID == 1219925"])
         print(scaled_x_df.query("index == 610"))
         print(scaled_x_df.query("index == 172"))
         # print(f"Mean of means of scaled_x_train {scaled_x_train.mean(axis=0).mean()}")
