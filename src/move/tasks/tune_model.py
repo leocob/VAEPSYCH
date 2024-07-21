1638ea98cb6feb935ea10ae0ee819a6fd1f14ece
leocob <leonardo.cobuccio@cpr.ku.dk>
2024-07-21 11:55:30 +0200

    add print

--- src/move/tasks/tune_model.py
+++ src/move/tasks/tune_model.py
@@ -204,9 +204,19 @@ def tune_model(config: MOVEConfig) -> float:
         print(f"PRINTING TUNE_RECONSTRUCTION")
         print(f"train_dataloader: {train_dataloader}")
         print(f"train_dataloader.dataset: {train_dataloader.dataset}")
+        # extract the data from the dataset
+        print(f"train_dataloader.dataset.cat_all: {train_dataloader.dataset.cat_all}")
+        print(f"train_dataloader.dataset.con_all: {train_dataloader.dataset.con_all}")
+        print(f"train_dataloader.dataset.cat_shapes: {train_dataloader.dataset.cat_shapes}")
+        print(f"train_dataloader.dataset.con_shapes: {train_dataloader.dataset.con_shapes}")
 
         print(f"test_dataloader: {test_dataloader}")
         print(f"test_dataloader.dataset: {test_dataloader.dataset}")
+        # extract the data from the dataset
+        print(f"test_dataloader.dataset.cat_all: {test_dataloader.dataset.cat_all}")
+        print(f"test_dataloader.dataset.con_all: {test_dataloader.dataset.con_all}")
+        print(f"test_dataloader.dataset.cat_shapes: {test_dataloader.dataset.cat_shapes}")
+        print(f"test_dataloader.dataset.con_shapes: {test_dataloader.dataset.con_shapes}")
 
         train_dataset = cast(MOVEDataset, train_dataloader.dataset)
 
