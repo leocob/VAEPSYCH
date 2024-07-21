4be9094836abd2bef32cc10a45a7477a3185ea41
leocob <leonardo.cobuccio@cpr.ku.dk>
2024-07-21 18:22:01 +0200

    trial config

--- src/move/data/io.py
+++ src/move/data/io.py
@@ -157,8 +157,7 @@ def read_tsv(
     dataset_name = basename(path)
     # remove extension
     dataset_name = dataset_name.split(".")[0]
-    config = MOVEConfig
-    interim_path = Path(config.data.interim_data_path)
+    interim_path = Path(MOVEConfig.data.interim_data_path)
     # print("Before filtering in read_tsv function")
     # print(data)
     # print(f"data.query(index == 610): {data.query('index == 610')}")
