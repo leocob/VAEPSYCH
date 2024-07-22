99e7871245f4f3709c9b08869e8f5dbe80678bc4
leocob <leonardo.cobuccio@cpr.ku.dk>
2024-07-22 15:41:01 +0200

    update print

--- src/move/models/vae.py
+++ src/move/models/vae.py
@@ -466,10 +466,10 @@ class VAE(nn.Module):
         epoch_bceloss = 0
 
         # show all the elements of train_loader
-        print(f"train_loader.size: {train_loader.size}")
+        print(f"len(train_loader): {len(train_loader)}")
         for index, (cat, con) in enumerate(train_loader):
                # Use the index value if needed
-            print(f"Index: {index}, cat.shape: {cat.shape}, con.shape: {con.shape}")
+            print(f"Index: {index}, Category: {cat.shape}, con.shape: {con.shape}")
 
             # Move input to GPU if requested
             cat = cat.to(self.device)
