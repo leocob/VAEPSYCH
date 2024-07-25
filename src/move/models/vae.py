b28c558389884f61f447bbe4861a3fa1129e185f
leocob <leonardo.cobuccio@cpr.ku.dk>
2024-07-25 13:28:46 +0200

    remove print batch size

--- src/move/models/vae.py
+++ src/move/models/vae.py
@@ -381,7 +381,6 @@ class VAE(nn.Module):
         
         e = 1e-8
         batch_size = con_in.shape[0]
-        print(f"batch_size: {batch_size}")
         total_shape = 0
         con_errors_list: list[torch.Tensor] = []
 
