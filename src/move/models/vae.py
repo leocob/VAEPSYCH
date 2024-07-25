49591cb717afeb90704eee1104d7756844e683ce
leocob <leonardo.cobuccio@cpr.ku.dk>
2024-07-25 11:45:13 +0200

    divide MSELoss masked by batch size

--- src/move/models/vae.py
+++ src/move/models/vae.py
@@ -382,42 +382,40 @@ class VAE(nn.Module):
         total_shape = 0
         con_errors_list: list[torch.Tensor] = []
 
-        print(f"con_in.shape: {con_in.shape}")
-        print(f"con_out.shape: {con_out.shape}")
-        print(f"con_in: {con_in}")
-        print(f"con_out: {con_out}")
-
-        print(f"mask.shape: {mask.shape}")
         # self.continuous_shapes: [51, 10, 1, 1, 11, 9, 9, 21, 8], it's the number of features for each continuous dataset
         # This part is to subset each dataset from the continuous data and calculate the MSE for each dataset
         for s in self.continuous_shapes:
             # For each continuous dataset, e.g. 51, 10, 1, 1, 11, 9, 9, 21, 8
 
-            print(f"con_in.shape: {con_in.shape}")
             c_in = con_in[:, total_shape : (s + total_shape - 1)]
-            print(f"c_in.shape: {c_in.shape}")
             c_re = con_out[:, total_shape : (s + total_shape - 1)]
             # c_mask = mask[:, total_shape : (total_shape + s)]
             c_mask = mask[:, total_shape : (s + total_shape - 1)]
-            print(f"c_mask.shape: {c_mask.shape}")
-
-            print(os.getcwd())
-            # save c_in numpy array to numpy file
-            np.save("c_in.npy", c_in.detach().cpu().numpy())
-            np.save("c_re.npy", c_re.detach().cpu().numpy())
-            np.save("c_mask.npy", c_mask.detach().cpu().numpy())
+            
+            ######################################## PRINTING ###########################################################
+            # print(f"con_in.shape: {con_in.shape}")
+            # print(f"c_in.shape: {c_in.shape}")
+            # print(f"c_mask.shape: {c_mask.shape}")
+            # print(os.getcwd())
+            # # save c_in numpy array to numpy file
+            # np.save("c_in.npy", c_in.detach().cpu().numpy())
+            # np.save("c_re.npy", c_re.detach().cpu().numpy())
+            # np.save("c_mask.npy", c_mask.detach().cpu().numpy())
+            ##############################################################################################################
             # error is the MSE loss
             valid_elements = c_mask.sum()
 
 
             if valid_elements > 0:
-                error = loss(c_re[c_mask], c_in[c_mask]) / valid_elements
+                error = loss(c_re[c_mask], c_in[c_mask]) / batch_size
+                # error = loss(c_re[c_mask], c_in[c_mask]) / valid_elements
             else:
                 error = torch.tensor(0.0, device=self.device)
             con_errors_list.append(error)
             total_shape += s
+
             # exit the program
-            exit()
+            # exit()
 
         # con_errors_list is a list of MSE for each continuous dataset? But I want to have 
         # print(f"len(con_errors_list): {len(con_errors_list)}") # 9
